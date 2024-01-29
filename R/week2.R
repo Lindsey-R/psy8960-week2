090bd538e274f2df991757ffefd20161ae42d357
Lindsey-R <105560190+Lindsey-R@users.noreply.github.com>
2024-01-29 14:16:36 -0600

    Added class notes

--- R/week2.R
+++ R/week2.R
@@ -1,5 +1,5 @@
 # Import and Labeling
-rt_df <- read.csv(file = "~/psy8960-week2/data/week2.csv", header = TRUE)
+rt_df <- read.csv(file = "data/week2.csv", header = TRUE)
 rt_df$condition <- factor(rt_df$condition, levels = c("A", "B"), labels = c("Control", "Experimental"))
 rt_df$gender <- factor(rt_df$gender, levels = c("M", "F", "N"), labels = c("Male", "Female", "Nonbinary"))
 
@@ -9,3 +9,6 @@ rt_f_df <- rt_df[rt_df$gender == "Female", ]
 hist(rt_f_df$rt)
 datasets <- list(rt_df, rt_f_df)
 datasets[[1]][2]
+# returns a data frame - [[1]] - extract the first item of a list; [2], subset dataframe
+# if datasets[[1]][[2]]: returns a vector
+# [ ] - subset; $ extract
